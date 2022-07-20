8bed5f9e6e175e7256b1a75d1d35161930fe4d98
Thorsten Lorenz <thlorenz@gmx.de>
2022-07-20 15:08:25 -0600

    s3: adding bucket to receive zip files

--- s3-unzip-fanout-nodejs/src/cdk-stack.ts
+++ s3-unzip-fanout-nodejs/src/cdk-stack.ts
@@ -1,8 +1,9 @@
-import { Stack, StackProps } from 'aws-cdk-lib'
+import { Stack, StackProps, aws_s3 as s3 } from 'aws-cdk-lib'
 import { Construct } from 'constructs'
 
 export class S3UnzipFanoutStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props)
+    new s3.Bucket(this, 'ZipfileBucket', { versioned: true })
   }
 }
